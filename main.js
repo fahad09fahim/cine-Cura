f8428600925531ae78b6cbddb41d195d9d8ccd6b
fahad09fahim <anik.fahim07@gmail.com>
2024-10-21 18:14:15 +0600

    searching functionality implemented

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
 const apiKey = "dcaebebf1342d25851db4b075b0be17e";
-const movieTitle = "The Nun";
 
 // search for movies details
-fetch(
-  `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${movieTitle}`
-)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data.results[0]);
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
 
+const movieData = async (searchText) => {
+  console.log(searchText);
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchText}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Movie not found");
+    }
+    const data = await response.json();
+    const movies = data.results[0];
+    // console.log(data.results);
+    displayMovie(movies);
+    if (movies.length === 0) {
+      alert("Phone not found");
+      location.reload();
+      return;
+    }
+  } catch (error) {
+    // Handle network or other errors
+    console.error(error);
+    alert(error.message || "An error occurred");
+  }
+};
 const options = {
   method: "GET",
   headers: {
@@ -38,16 +52,16 @@ const displayTopMovies = (data) => {
   movies = movies.slice(0, 6);
 
   movies.forEach((movie) => {
-    console.log(movie);
+    // console.log(movie);
     let voteAvg = movie.vote_average.toFixed(2);
     const movieCard = document.createElement("div");
     movieCard.innerHTML = `
 <div class="card bg-[#50727B] w-80  shadow-xl indicator ">
-<span class="indicator-item indicator-bottom indicator-start ml-4 badge badge-success ">⭐ ${voteAvg}</span>
+<span class="indicator-item indicator-bottom indicator-start ml-6 badge badge-success ">⭐ ${voteAvg}</span>
   <figure>
     <img
       src="https://image.tmdb.org/t/p/w400${movie?.backdrop_path}"
-      alt="Shoes" />
+      alt="movie poster" />
   </figure>
   <div class="p-4">
     <h2 class="text-lg font-semibold truncate">${movie?.title}</h2>
@@ -57,4 +71,39 @@ const displayTopMovies = (data) => {
  `;
     movieContainer.appendChild(movieCard);
   });
+};
+// handle movie search
+const handleSearch = () => {
+  const searchField = document.getElementById("search-movie");
+  const searchText = searchField.value;
+  // console.log(searchText);
+  movieData(searchText);
+};
+
+// handle movie search results
+const displayMovie = (movie) => {
+  const resultContainer = document.getElementById("result-container");
+  const movieCard = document.createElement("div");
+  resultContainer.textContent = " ";
+  movieCard.innerHTML = `
+    <div class="card lg:card-side bg-[#50727B] shadow-xl  ">
+  <figure>
+    <img
+    class="w-full"
+      src="https://image.tmdb.org/t/p/w400/${movie.poster_path}"
+      alt="Album" />
+  </figure>
+  <div class="card-body ">
+    <h2 class="text-center text-3xl">${movie.title}</h2>
+    <h4>overview: ${movie.overview}</h4>
+    <h4>Release Date: ${movie.release_date}</h4>
+    <h4>Rating: ${movie.vote_average.toFixed(2)}</h4>
+    <h4>Total vote: ${movie.vote_count}</h4>
+    <h4>language: ${movie.original_language.toUpperCase()}</h4>
+  </div>
+</div>
+
+    `;
+  resultContainer.appendChild(movieCard);
+  console.log(movie);
 };
